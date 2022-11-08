aa441ebba58a742cb0e34eeb943f4763c3423855
Philipp Herz <philippherz@gmx.de>
2022-11-08 10:48:20 +0100

    init

---
+++ better9gag.ts
@@ -0,0 +1,35 @@
+var styles = `
+.localSave-Button{
+    cursor: pointer;
+}
+`
+
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
+
+const checkedPosts = new Array<string>();
+function createLocalSave(parent: HTMLElement) {
+  const localSave = document.createElement("div");
+  localSave.classList.add("localSave-Button");
+  localSave.dataset.article = parent.id;
+  localSave.id = "localSave-" + parent.id;
+  const saveImg = document.createElement("img");
+  saveImg.src = "";
+  localSave.appendChild(saveImg);
+  checkedPosts.push(parent.id);
+  console.log()
+  return localSave;
+}
+
+function onLoad() {
+  let saveHeaders = document.querySelectorAll(".post-header");
+  console.debug(saveHeaders);
+  saveHeaders.forEach((value) => {
+    var parent = value.parentElement?.parentElement;
+    if (parent && !checkedPosts.includes(parent.id))
+      value.appendChild(createLocalSave(parent));
+  });
+}
+console.log("Loading Local");
+setInterval(() => onLoad(), 1000);
